5f789c00930357b0e756cbb088f12d314a95a395
bacil1933 <acilror6@gmail.com>
2026-05-07 11:01:45 +0700

    Fix product image paths and clean up code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 const products = [
-    { id: 1, name: 'beng beng', price: 2000, img: 'img/beng beng.jpg' },
-    { id: 2, name: 'boncabe', price: 1000, img: 'img/boncabe.png' },
-    { id: 3, name: 'chocopie', price: 2000, img: 'img/chocopie.jpg' },
-    { id: 4, name: 'maxicorn', price: 2000, img: 'img/maxicorn.jpg' },
-    { id: 5, name: 'qtella', price: 2000, img: 'img/qtella.png' },
+    { id: 1, name: 'beng beng', price: 2000, img: 'beng beng.jpg' },
+    { id: 2, name: 'boncabe', price: 1000, img: 'boncabe.png' },
+    { id: 3, name: 'chocopie', price: 2000, img: 'chocopie.jpg' },
+    { id: 4, name: 'maxicorn', price: 2000, img: 'maxicorn.jpg' },
+    { id: 5, name: 'qtella', price: 2000, img: 'qtella.png' },
 ];
 
 // Keranjang belanja
